956ad78e8c224fa205e0f864a62d112d666992b9
Engkos <engkoskostaman759@gmail.com>
2022-12-28 15:24:03 +0700

    mendispatch action reducer

--- src/Cointainer.js
+++ src/Cointainer.js
@@ -2,11 +2,12 @@ import React from 'react';
 import {connect} from 'react-redux';
  
  function Cointainer(props) {
+  console.log('render =>', props);
    return (
      <div>
        <h1>Saya Mesin Penghitung</h1>
        <p>Count : {props.count}</p>
-       <button>Increment</button>
+       <button onClick={props.onIncrementClick}>Increment</button>
      </div>
    )
  }
@@ -16,6 +17,14 @@ import {connect} from 'react-redux';
     count: state.count
   }
  }
-
- export default  connect(mapStatetoProps)(Cointainer);
+ function mapDispatchtoProps(dispatch){
+  return {
+    onIncrementClick : () =>{
+      console.log('tombol di click');
+      const action ={ type : 'INCREMENT'};
+      dispatch(action);
+    }
+  }
+ }
+ export default  connect(mapStatetoProps,mapDispatchtoProps)(Cointainer);
  
